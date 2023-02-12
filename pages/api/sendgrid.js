d50a427e92f8a18941d2c7b68ead1392ea5684a8
Brayden <dudgjs1318@gmail.com>
2023-02-12 00:01:10 -0800

    update api key

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,8 +6,8 @@ async function sendEmail(req, res) {
   try {
     console.log("REQ.BODY", req.body);
     await sendgrid.send({
-      to: 'dudgjs1318@gmail.com',
-      from: "dudgjs1318@gmail.com",
+      to: 'braydenkim98@gmail.com',
+      from: "braydenkim98@gmail.com",
       subject: ` ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
